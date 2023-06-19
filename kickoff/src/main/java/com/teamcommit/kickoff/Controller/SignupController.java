48c7c132f4de46e94690eba074b5b8a966298066
likeein <ain91201@gmail.com>
2023-06-19 16:22:38 +0900

    reply

--- kickoff/src/main/java/com/teamcommit/kickoff/Controller/SignupController.java
+++ kickoff/src/main/java/com/teamcommit/kickoff/Controller/SignupController.java
@@ -1,5 +1,7 @@
 package com.teamcommit.kickoff.Controller;
 
+import java.util.Random;
+
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,25 +10,51 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.teamcommit.kickoff.Service.login.CertificationService;
 import com.teamcommit.kickoff.Service.login.LoginService;
+import com.teamcommit.kickoff.Service.signup.SignupService;
 
 @Controller
 public class SignupController {
 
-	/*
-	 * @Autowired
-	 * 
-	 * @Qualifier("LoginService") private LoginService loginService;
-	 * 
-	 * @RequestMapping("/Signup") public String Signup() { String view =
-	 * "/login/Signup";
-	 * 
-	 * return view; }
-	 * 
-	 * @RequestMapping("/SignupEmp") public String SignupEmp() { String view =
-	 * "/login/SignupEmp";
-	 * 
-	 * return view; }
-	 */
+	/*@Autowired
+	@Qualifier("SignupService")
+	private SignupService signupService;
+
+	//사용자 회원가입 페이지 이동
+	@GetMapping("/Signup")
+	public String Signup() {
+		String view = "/login/Signup";
+
+		return view;
+	}
+
+	//업체 회원가입 페이지 이동
+	@GetMapping("/SignupEmp")
+	public String SignupEmp() {
+		String view = "/login/SignupEmp";
+
+		return view;
+	}*/
+	
+	/*// coolsms api
+    @GetMapping("/check/sendSMS")
+    public @ResponseBody String sendSMS(String phoneNumber) {
+
+    	// 4자리 난수 생성
+        Random rand  = new Random();
+        String numStr = "";
+        for(int i=0; i<4; i++) {
+            String ran = Integer.toString(rand.nextInt(10));
+            numStr+=ran;
+        }
+        // CertificationService를 사용하여 SMS 전송
+        CertificationService.certifiedPhoneNumber(phoneNumber,numStr);
+        
+        // 생성된 4자리 난수 반환
+        return numStr;
+    }*/
+
 }
--- kickoff/src/main/java/com/teamcommit/kickoff/Service/signup/SignupServiceImpl.java
+++ kickoff/src/main/java/com/teamcommit/kickoff/Service/signup/SignupServiceImpl.java
@@ -1,5 +1,11 @@
 package com.teamcommit.kickoff.Service.signup;
 
-public class SignupServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service("signupService")
+public class SignupServiceImpl implements SignupService{
+
+	@Autowired
+	private SignupMapper signupMapper;
 }
