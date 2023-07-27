cd11fe5f051228f80795ee583d47a0151f5dd833
yeonu <yyeonunu@gmail.com>
2023-07-27 14:28:55 +0900

    yeonu

--- kickoff/src/main/java/com/teamcommit/kickoff/yeonu/TeamRecruiterServiceImpl.java
+++ kickoff/src/main/java/com/teamcommit/kickoff/yeonu/TeamRecruiterServiceImpl.java
@@ -1,11 +1,13 @@
 package com.teamcommit.kickoff.yeonu;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.teamcommit.kickoff.Do.TeamApplyDO;
+import com.teamcommit.kickoff.Do.TeamInfoDO;
+import com.teamcommit.kickoff.Do.UserDO;
 
 @Service("TeamRecruiterService")
 public class TeamRecruiterServiceImpl implements TeamRecruiterService {
@@ -13,9 +15,33 @@ public class TeamRecruiterServiceImpl implements TeamRecruiterService {
 	@Autowired
 	private TeamRecruiterMapper teamRecruiterMapper;
 	
-	//지원자 리스트 & 상세보기
+	//팀 정보
 	@Override
-	public List<TeamApplyDO> teamApplyList(String userId) {
-		return teamRecruiterMapper.teamApplyList(userId);
+	public TeamInfoDO teamInfo(String userId) {
+		return teamRecruiterMapper.teamInfo(userId);
+	}
+
+	//모집글 별 지원자 수
+//	@Override
+//	public TeamApplyDO applyCount(int applyCount) {
+//		return teamRecruiterMapper.applyCount(applyCount);
+//	}
+	
+	//모집글 리스트
+	@Override
+	public List<Map<String, String>> recruitInfoList(String userId) throws Exception {
+		return teamRecruiterMapper.recruitInfoList(userId);
+	}
+	
+	//모집글 별 지원자 리스트
+	@Override
+	public List<Map<String, String>> teamApplyList(String rNum) throws Exception {
+		return teamRecruiterMapper.teamApplyList(rNum);
+	}
+	
+	//지원자 정보
+	@Override
+	public List<UserDO> applyInfo(String userId) throws Exception {
+		return teamRecruiterMapper.applyInfo(userId);
 	}
 }
--- kickoff/src/main/resources/static/apply/js/applyTeam.js
+++ kickoff/src/main/resources/static/apply/js/applyTeam.js
@@ -1,15 +1,74 @@
 /* apply team */
 
-/* checkbox */
-function drop() {
-	var drop = document.getElementById('drop');
-	var detail = document.getElementById('detail');
-
-	if (drop.checked == true) {
-		detail.style.display = "block";
-	} else {
-		detail.style.display = "none";
+/* apply status */
+var statusDivs = document.getElementsByClassName("status");
+for (var i = 0; i < statusDivs.length; i++) {
+    var statusDiv = statusDivs[i];
+    
+    if (statusDiv.innerText === "모집 완료") {
+        statusDiv.style.borderColor = "#00A168";
+        statusDiv.style.backgroundColor = "#00A168";
+    } else if (statusDiv.innerText === "모집 중") {
+		statusDiv.style.borderColor = "#1970FF";
+        statusDiv.style.backgroundColor = "#1970FF";
 	}
 }
 
+/* checkbox */
+function drop(num) {
+ 	var dropCheck = $('#dropCheck'+num).val();
+	if(dropCheck == "true") {
+		var drop = document.getElementById('drop'+num);
+		var detail = document.getElementById('detail'+num);
+	
+		if (drop.checked == true) {
+			detail.style.display = "block";
+		} else {
+			detail.style.display = "none";
+		}
+		
+	} else {
+		$('#checkbox'+num);
+		alert('해당 풋살장 예약글에 신청자가 없습니다.');
+	}
+}
+    
+/* side-bar open */
+function sideon() {
+	$(".side-container").addClass("menuon");
+	$(".sStatus").hide();
+}
 
+/* side-bar close */
+function btnSide() {
+	$(".side-container").toggleClass("menuon");
+	$(".sStatus").hide();
+}
+
+/* side-bar info */
+function ajaxSidebar(userId, num) {
+	var userId = $('#userId'+num).val();
+	
+    $.ajax({
+        url: "/apply-ajax",
+        type: "GET",
+        dataType: "json",
+        data: { userId: userId },
+        success: function(data) {
+            $("#sideBar #applyUserId").html(data.userId + "/" + data.userName);
+            console.log(data.userId, data.userName);
+			$("#sideBar #userGender").html(data.userGender);
+			$("#sideBar #userAge").html(data.userAge);
+			if(data.userSubPosition != null) {
+        		var position = data.userMainPosition + "/" + data.userSubPosition;
+        	} else {
+        		var position = data.userMainPosition + "/" + "-";
+        	}
+			$("#sideBar #userPosition").html(position);
+			$("#sideBar #userLocal").html(data.userAddress);
+        },
+        error: function(error) {
+            alert('Ajax 요청이 실패했습니다. 오류: ' + error);
+        }
+    });
+}
